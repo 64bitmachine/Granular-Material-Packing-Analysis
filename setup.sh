63302023d5739298a4eb5cb8e20ea18bceac71ce
64bitmachine <85003035+64bitmachine@users.noreply.github.com>
2022-05-12 16:50:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,15 @@
 #     export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 # fi
 
-
 pip3 install scikit-image
 pip3 install SimpleITK
 pip3 install itk
+pip3 install vtk
+sudo apt install libboost-numpy-dev
+sudo apt-get install build-essential
+sudo apt-get install libboost-all-dev
+sudo apt-get install freeglut3-dev
+
+
+# in case anaconda is used
+sudo ln -s /home/tejas/anaconda3/lib/python3.9/site-packages/numpy/core/include/numpy/ /usr/include/numpy
